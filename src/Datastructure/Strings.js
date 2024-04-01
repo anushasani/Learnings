ba415de76ba4444b212bbcf836e8107b607ad4df
Anusha <anusha.sani55@gmail.com>
2024-04-01 10:09:13 -0700

    runningTotal

--- src/Datastructure/Strings.js
+++ src/Datastructure/Strings.js
@@ -1,14 +1,15 @@
-// Write a function that returns the elements on odd positions in a list
-function returnOdd(arr) {
-  const temp = [];
+// Write a function that computes the running total of a list
+
+function sumOfList(arr) {
+  let sum = 0;
+  let runningTotalList = [];
   for (let i = 0; i < arr.length; i++) {
-    if (i % 2 !== 0) {
-      temp.push(arr[i]);
-    }
+    sum += arr[i];
+    runningTotalList.push(sum);
   }
-  return temp;
+  return runningTotalList;
 }
 
-const arr = [45, 76, 2, 1, 4, 5, 35, 45];
+const arr = [1, 2, 3, 4, 5];
 
-console.log("Odd Number in the list are : " + returnOdd(arr));
+console.log("Total of a List is : ", sumOfList(arr));
