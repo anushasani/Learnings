609db3a4dc99dad3e70cf6d893d1d60dbecee4e8
Anusha <anusha.sani55@gmail.com>
2024-04-01 15:04:34 -0700

    Factorial

--- src/Datastructure/Strings.js
+++ src/Datastructure/Strings.js
@@ -1,31 +1,14 @@
-// Write three functions that compute the sum of the numbers in a list: using a for-loop, a while-loop and recursion. (Subject to availability of these constructs in your language of choice.)
+// Factorial of a number ;
 
-function sumOfArray(arr) {
-  let sum = 0;
-  for (let i = 0; i < arr.length; i++) {
-    sum += arr[i];
+function factorial(n) {
+  if (n === 0) {
+    return 1;
   }
-  return sum;
-}
-
-function sumOfArray(arr) {
-  let i = 0;
-  let sum = 0;
-
-  while (i < arr.length) {
-    sum += arr[i];
-    i++;
-  }
-  return sum;
-}
-
-function sumOfArray(arr, sum = 0) {
-  if (arr.length === sum) {
-    return 0;
+  if (n === 1) {
+    return 1;
   }
 
-  return arr[sum] + sumOfArray(arr, sum + 1);
+  return n * factorial(n - 1);
 }
 
-let arr = [1, 2, 3, 4, 5];
-console.log("Total sum is ", sumOfArray(arr));
+console.log("factorial of n is :", factorial(3));
