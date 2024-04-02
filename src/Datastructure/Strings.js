df0e7e758bf4bafc67989db9a22c8e486669514f
Anusha <anusha.sani55@gmail.com>
2024-04-01 17:49:26 -0700

    perfectSquare

--- src/Datastructure/Strings.js
+++ src/Datastructure/Strings.js
@@ -1,14 +1,13 @@
-// Factorial of a number ;
+// Write a function on_all that applies a function to every element of a list. Use it to print the first twenty perfect squares. The perfect squares can be found by multiplying each natural number with itself. The first few perfect squares are 1*1= 1, 2*2=4, 3*3=9, 4*4=16. Twelve for example is not a perfect square because there is no natural number m so that m*m=12. (This question is tricky if your programming language makes it difficult to pass functions as arguments.)
 
-function factorial(n) {
-  if (n === 0) {
-    return 1;
+function on_all(n) {
+  let value = 0;
+  for (let i = 0; i <= 20; i++) {
+    if (Math.floor(Math.sqrt(i)) * Math.floor(Math.sqrt(i)) === i) {
+      value = i;
+      console.log(value);
+    }
   }
-  if (n === 1) {
-    return 1;
-  }
-
-  return n * factorial(n - 1);
 }
 
-console.log("factorial of n is :", factorial(3));
+console.log(on_all(20));
