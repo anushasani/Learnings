4b14d07f319d96e39e31aee7e8c3422abc784264
Anusha <anusha.sani55@gmail.com>
2024-04-02 13:17:04 -0700

    concatList

--- src/Datastructure/Strings.js
+++ src/Datastructure/Strings.js
@@ -1,18 +1,33 @@
 // Write a function that concatenates two lists. [a,b,c], [1,2,3] → [a,b,c,1,2,3]
 
+//  One Method
 function concatenateLists(list1, list2) {
   let temp = [];
   for (let i = 0; i < list1.length; i++) {
-    temp.push(list1[i].concat(list2[i]));
+    temp.push(list1[i]);
+    temp.push(list2[i]);
   }
   return temp;
-}
-
-function listsSplit(list1, list2) {
-  return list1.concat(list2);
 }
 
 const list1 = ["a", "b", "c"];
 const list2 = [1, 2, 3];
 console.log(concatenateLists(list1, list2));
-console.log(listsSplit(list1, list2));
+console.log(splitMethod(list1, list2));
+
+function splitMethod(list1, list2) {
+  let l = list1.length + list2.length;
+
+  let r = [];
+  let k = 0;
+  let a = 0;
+
+  for (let i = 0; i < l; i++) {
+    if (i % 2 === 0) {
+      r[i] = list1[k++];
+    } else {
+      r[i] = list2[a++];
+    }
+  }
+  return r;
+}
