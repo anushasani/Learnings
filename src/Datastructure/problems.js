faa9b29fd41dad6576284ba54f801baa92efdf43
Anusha <anusha.sani55@gmail.com>
2024-04-06 16:58:16 -0700

    twoSum

--- src/Datastructure/problems.js
+++ src/Datastructure/problems.js
@@ -1,47 +1,16 @@
-//Brutfore code for Highest Common factor or Greatest Common divisor :
+//finding Sum of two in Arrys :
 
-// function HCF(a, b) {
-//   let min = 0;
+function sumOfTwo(nums, target) {
+  const map = new Map();
 
-//   if (a < b) {
-//     min = a;
-//   } else {
-//     min = b;
-//   }
+  for (const index in nums) {
+    const pairValue = target - nums[index];
+    if (map.has(pairValue)) return [map.get(pairValue), index];
 
-//   for (let i = min; i >= 1; i--) {
-//     if (a % i === 0 && b % i === 0) {
-//       return i;
-//     }
-//   }
-// }
-
-// console.log("Heighest Common factor", HCF(20, 15));
-//Eculied Algorithm says : find a largest number and
-// replace the LN with  difference between smallest and largest
-//Repeat step 1 and step 2 till both numbers are equal
-//Return any one number
-
-function HCFByEuclids(a, b) {
-  while (a !== 0 && b !== 0) {
-    if (a > b) {
-      a = a % b;
-    } else {
-      b = b % a;
-    }
-  }
-  if (a !== 0) {
-    return a;
-  } else {
-    return b;
+    map.set(nums[index], index);
   }
 }
+const nums = [2, 7, 11, 15];
+const target = 9;
 
-console.log("Highest Comman factor by eculids: ", HCFByEuclids(45, 78));
-
-//12 8
-//4  8
-//4 0
-
-// a/2 or b/2 > a%2 or b%2
-//  log(min(a,b))
+console.log(sumOfTwo(nums, target));
