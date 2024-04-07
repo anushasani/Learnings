cf7f9f1dc0723cf7f3b707e00455500c7ee9ec43
Anusha <anusha.sani55@gmail.com>
2024-04-07 08:27:53 -0700

    duplicateValue

--- src/Datastructure/problems.js
+++ src/Datastructure/problems.js
@@ -1,16 +1,19 @@
 //finding Sum of two in Arrys :
 
-function sumOfTwo(nums, target) {
+function dupblicateContains(nums) {
   const map = new Map();
 
   for (const index in nums) {
-    const pairValue = target - nums[index];
-    if (map.has(pairValue)) return [map.get(pairValue), index];
-
-    map.set(nums[index], index);
+    if (map.has(nums[index])) return true;
+    map.set(nums[index]);
   }
+  return false;
 }
-const nums = [2, 7, 11, 15];
-const target = 9;
+const nums = [1, 2, 3];
 
-console.log(sumOfTwo(nums, target));
+console.log(dupblicateContainsSet(nums));
+
+function dupblicateContainsSet(nums) {
+  const set = new Set(nums);
+  return set.size !== nums.length;
+}
